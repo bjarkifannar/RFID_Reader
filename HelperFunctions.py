14f91e9e39770cfdfc08cb71db1e0f1e2cafa216
bjarkifannar <bjarkifannar@gmail.com>
2017-11-27 13:41:19 +0000

    Added comments to HelperFunctions.py and UserFunctions.py

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -1,107 +1,133 @@
+# Imports
 import Globals
 
 import time
 import json
 
+# Returns the current time
 def time_get():
-	return time.strftime("%w %H:%M", time.gmtime())
+	return time.strftime("%w %H:%M", time.gmtime()) # Time format: <Day of the week> <Hours>:<Minutes>
 
+# Fixes a JSON string and returns it
 def fix_json(j):
-	j = str(j)
+	j = str(j) # Make sure it's a string
 	return j.replace(' u\'', ' "').replace('{u\'', '{"').replace('\'', '"').replace('"{', '{').replace('}"', '}')
 
+# Resets labels in the app
 def reset_labels():
 	Globals.app.clearLabel('msg')
 
+# Marks attendance for the user
 def attendance():
 	try:
+		# Open data files
 		timetableFile = open("Data/Timetable.json", "r")
 		AttFile = open("Data/Attendance.json", "r")
+
+		# Load the JSON
 		ttData = json.load(timetableFile)
 		attData = json.load(AttFile)
+
+		# Get the time
 		cur_time = time_get()
 		cur_day = cur_time.split(' ')[0]
 		time_h_m = cur_time.split(' ')[1]
+
+		# Class info
 		day_class = ""
 		class_name = ""
 		class_group = ""
 		in_week = ""
 		
-		if time_h_m >= Globals.start_times[0] and time_h_m <= Globals.end_times[0]:
+		if time_h_m >= Globals.start_times[0] and time_h_m <= Globals.end_times[0]: # 1st class of the day
 			day_class = "0"
-		elif time_h_m >= Globals.start_times[1] and time_h_m <= Globals.end_times[1]:
+		elif time_h_m >= Globals.start_times[1] and time_h_m <= Globals.end_times[1]: # 2nd class of the day
 			day_class = "1"
-		elif time_h_m >= Globals.start_times[2] and time_h_m <= Globals.end_times[2]:
+		elif time_h_m >= Globals.start_times[2] and time_h_m <= Globals.end_times[2]: # 3rd class of the day
 			day_class = "2"
-		elif time_h_m >= Globals.start_times[3] and time_h_m <= Globals.end_times[3]:
+		elif time_h_m >= Globals.start_times[3] and time_h_m <= Globals.end_times[3]: # 4th class of the day
 			day_class = "3"
 		
-		class_name = ttData[cur_day][day_class]["class"]
+		class_name = ttData[cur_day][day_class]["class"] # Get the class name
 		
+		# If the class name is not empty
 		if not class_name == "":
-			class_group = ttData[cur_day][day_class]["group"]
-			in_week = ttData[cur_day][day_class]["in_week"]
+			class_group = ttData[cur_day][day_class]["group"] # Get the class group
+			in_week = ttData[cur_day][day_class]["in_week"] # Is it the first or second time in the week for this class?
 			
-			class_n_g = class_name + '-' + class_group
+			class_n_g = class_name + '-' + class_group # The class name and group
 			
-			attData[class_n_g][in_week][Globals.user_ssn.encode("utf-8")] = "m"
+			attData[class_n_g][in_week][Globals.user_ssn.encode("utf-8")] = "m" # Make the attendance be "m" for the user in this class
 			
+			# Open the attendance file and write the new data in it
 			AttFile = open("Data/Attendance.json", "w")
 			AttFile.write(fix_json(attData))
-	except Exception, ex:
+	except Exception, ex: # If someting went wrong
+		# Print the error
 		print "ERROR!"
 		print ex
 
+# Draws the timetable
 def draw_timetable():
 	try:
+		# Open the timetable file and get the JSON
 		timetableFile = open("Data/Timetable.json", "r")
 		data = json.load(timetableFile)
 		
+		# Start a label frame, set the background to white and make it stick to all sides
 		Globals.app.startLabelFrame("Timetable", 0, 0, 1, 3)
 		Globals.app.setLabelFrameBg("Timetable", "white")
 		Globals.app.setSticky("news")
 		
+		# A for loop for the days in the week
 		for i in range(1, 6):
-			data2 = data[str(i)]
+			data2 = data[str(i)] # Data for each day
+
+			# If there is at least one class that day
 			if not data2 == "{}":
-				for d2 in data2:
-					dat = data2[d2]
-					starts = int(dat["starts"][:-3])
-					string = dat["starts"] + " - " + dat["ends"] + "\n"
-					noClass = True
+				for d2 in data2: # For each class in the day
+					dat = data2[d2] # Get the data
+					starts = int(dat["starts"][:-3]) # Get the start hour
+					string = dat["starts"] + " - " + dat["ends"] + "\n" # The start and end time string
+					noClass = True # Is there a class at this time?
 					
-					if dat["class"] == "":
-						string = "Enginn tími"
-					else:
-						noClass = False
-						string = dat["class"] + "-" + dat["group"] + "\n" + dat["teacher"]
+					if dat["class"] == "": # If there is no class
+						string = "Enginn tími" # Add a message to the row/column
+					else: # If there is a class
+						noClass = False # There is a class
+						string = dat["class"] + "-" + dat["group"] + "\n" + dat["teacher"] # Add the class info to the row/column
 					
-					r = 0
+					r = 0 # What row to put the data in
 					
-					if starts == 8:
-						r = 0
-					elif starts == 10:
-						r = 1
-					elif starts == 13:
-						r = 2
-					elif starts == 15:
-						r = 3
+					if starts == 8: # If the start hour is 8
+						r = 0 # Row 0
+					elif starts == 10: # If the start hour is 10
+						r = 1 # Row 1
+					elif starts == 13: # If the start hour is 13
+						r = 2 # Row 2
+					elif starts == 15: # If the start hour is 15
+						r = 3 # Row 3
 					
+					# Titles for label frame and label
 					lfTitle = "lf_" + str(i) + "_" + str(r)
 					lTitle = "l_" + str(i) + "_" + str(r)
 					
+					# Start a label frame and change the title
 					Globals.app.startLabelFrame(lfTitle, r, i)
 					Globals.app.setLabelFrameTitle(lfTitle, dat["starts"] + " - " + dat["ends"])
 					
-					if noClass:
-						Globals.app.setLabelFrameBg(lfTitle, "green")
-					else:
-						Globals.app.setLabelFrameBg(lfTitle, "yellow")
+					if noClass: # If there is no class
+						Globals.app.setLabelFrameBg(lfTitle, "green") # Set the background to green
+					else: # If there is a class
+						Globals.app.setLabelFrameBg(lfTitle, "yellow") # Set the background to yellow
 					
+					# Add the message to the label frame and close it
 					Globals.app.addLabel(lTitle, string, r, i)
 					Globals.app.stopLabelFrame()
 		
+		# Close the timetable label frame
 		Globals.app.stopLabelFrame()
-	except Exception, ex:
+	except Exception, ex: # If something went wrong
+		# Print the error
 		print "ERROR!"
 		print ex
--- UserFunctions.py
+++ UserFunctions.py
@@ -1,3 +1,4 @@
+# Imports
 import Globals
 import HelperFunctions
 
@@ -5,38 +6,46 @@ import io
 import codecs
 import json
 
+# Gets the user and returns the info
 def get_user():
-	print Globals.uidStr
+	print Globals.uidStr # Prints the User ID string to the terminal (Good for debugging)
 	
-	message = ''
+	message = '' # Create an empty message
 	
 	try:
+		# Open the user file and get the JSON data
 		userFile = open("Data/Users.json", "r")
 		userData = json.load(userFile)
 		
-		data = userData[Globals.uidStr]
+		data = userData[Globals.uidStr] # Get the data for this user
 		
-		Globals.user_ssn = data["ssn"]
-		message = message + data['name'] + '\n' + data['email'] + '\n' + data['ssn']
+		Globals.user_ssn = data["ssn"] # The user's SSN (Kennitala)
+		message = message + data['name'] + '\n' + data['email'] + '\n' + data['ssn'] # Add the user info to the message
 		
-		HelperFunctions.attendance()
-	except Exception, ex:
+		HelperFunctions.attendance() # Mark the attendance for the user
+	except Exception, ex: # If something went wrong
+		# Let the user know that he/she is not found
 		message = 'User not found'
-		print ex
+		print ex # Print an error message if it's there
 	
-	Globals.app.setLabel('msg', message)
+	Globals.app.setLabel('msg', message) # Show the message
 
+# Adds a user
 def add_user():
+	# Gets the user's details
 	n_ssn = raw_input("Enter your ssn (Kennitala): ")
 	n_name = raw_input("Enter your name: ")
 	n_email = raw_input("Enter your email: ")
 	
+	# Open the user file and get the info
 	userFile = open("Data/Users.json", "r")
-	jsonData = json.dumps(userFile.read(), ensure_ascii=False)
+	jsonData = json.dumps(userFile.read(), ensure_ascii=False) # Make sure it's using UTF-8
 	userDataInitial = json.loads(jsonData)
 	
+	# Encode the JSON data to a UTF-8 string and fix it
 	userDataInitial = userDataInitial.encode("utf-8")
 	userDataStr = HelperFunctions.fix_json(userDataInitial.rstrip()[:-1]) + ', "%s": {"ssn": "%s", "name": "%s", "email": "%s"}}' % (Globals.uidStr, n_ssn, n_name, n_email)
 	
+	# Open the user file and write the data
 	userFile = open("Data/Users.json", "w")
 	userFile.write(userDataStr)
