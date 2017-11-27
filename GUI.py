36bd7f6e623bf80f0fc4768d3aab9145ba7d343c
Stimmikex <styrmir97@gmail.com>
2017-11-27 13:41:38 +0000

    Added Comments for the Globals and GUI

--- GUI.py
+++ GUI.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
-
+# Imports 
 import Globals
 import HelperFunctions
 import UserFunctions
@@ -15,6 +13,7 @@ import io
 import codecs
 from appJar import gui
 
+# Initialization of the global veribles
 Globals.init()
 
 # Capture SIGINT for cleanup when the script is aborted
@@ -53,24 +52,27 @@ def start_loop(action):
 			
 			# Select the scanned tag
 			MIFAREReader.MFRC522_SelectTag(Globals.uid)
-			
+			# Sets a timer.
 			time.sleep(5)
 
+# Initializing the "app"
 def init_app():
+	# Uses the helperfunctions to draw the timetable. 
 	HelperFunctions.draw_timetable()
-	
+	# Start the label frame.
 	Globals.app.startLabelFrame("Message", 3, 0)
-	
+	# Sets the label fram sticky, this will make the frame stick to all corners.
 	Globals.app.setSticky("news")
-	
+	# Addes a empty label fram, sets the label fram with the width of 700.
 	Globals.app.addEmptyLabel('msg')
 	Globals.app.setLabelWidths('msg', 700)
-	
+	# Ends the label fram.
 	Globals.app.stopLabelFrame()
-	
+	# Calls the start_loop function.
 	start_loop(0)
 
-Globals.app.setGeometry("800x600")
+# Sets the width and height of the fram, then is stretchs the fram, start the init_app function on a new thread, starts "app".
+.Globals.app.setGeometry("800x600")
 Globals.app.setStretch("both")
 Globals.app.thread(init_app)
 Globals.app.go()
--- Globals.py
+++ Globals.py
@@ -1,5 +1,5 @@
 from appJar import gui
-
+# These are all the global variables that we using in the code.
 def init():
 	global app
 	global continue_reading
@@ -9,10 +9,16 @@ def init():
 	global end_times
 	global user_ssn
 
+	# Title for the window, the "app" is the window.
 	app = gui("RFID Reader")
 	continue_reading = True
+	# The user id.
 	uid = 0
+	# This is hte user id turned into a string.
 	uidStr = ""
+	# Start time for the timetable, Start of the class.
 	start_times = ("08:00", "10:25", "13:00", "15:20")
+	# End time for the timetable, End of the class
 	end_times = ("10:15", "12:40", "15:15", "17:35")
+	# This is where the social security number is kept.
 	user_ssn = ""
